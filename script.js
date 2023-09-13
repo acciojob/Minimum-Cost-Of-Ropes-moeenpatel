548c7a2f1eecc6e7f535a364613cd57f3e9a21ab
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-13 21:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+    let arr=[4,2,7,6,9];
+	let n=arr.length-1;
+	let ans=0;
+	for(let i=1;i<=n;i++){
+		arr.sort((a,b)=>a-b);
+		let sum=(arr[0]+arr[1]);
+        arr.splice(0,2,sum);
+
+        ans+=sum;
+  }
+	console.log( ans);
 }  
